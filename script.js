3d13ea42f8676219186d49ee66b37a952497936b
Finn <finnterdal@gmail.com>
2019-07-10 10:31:25 -0500

    whoa, it is both easier and harder to DOM leak than originally expected

--- script.js
+++ script.js
@@ -10,13 +10,32 @@ const form = document.getElementById('dom-nodes-form')
 //   }
 // }, 2000)
 
+// const arrOfDivs = []
+
 form.addEventListener('submit', function(event) {
   event.preventDefault()
   const [{ value: numNodes }] = event.target
   console.log(numNodes)
   for (let i = 0; i < numNodes; i++) {
+    function hello() {}
+    setInterval(
+      function() {
+        // hello()
+      },
+      10000
+    )
     const newNode = document.createElement('DIV')
+    newNode.addEventListener('click', hello)
     newNode.classList.add('node')
     box.appendChild(newNode)
+    // arrOfDivs.push(newNode)
   }
+})
+
+box.addEventListener('click', function() {
+  box.innerHTML = ''
+  // while (box.firstElementChild) {
+  //   box.removeChild(box.firstElementChild)
+  // }
+  console.log(box.innerHTML)
 })
